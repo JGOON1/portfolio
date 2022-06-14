d45ff7a28366fb66f36fd24810f9701b84fa056f
john meng <john.meng014@gmail.com>
2022-06-14 15:36:11 -0400

    projects and skills basic structure

---
+++ src/data.js
@@ -0,0 +1,27 @@
+export const projects = [
+    {
+        title: "project 1",
+        subtitel: "React",
+        description: "description about the project",
+        image: "img or gif",
+        link: "link to image"
+    },
+    {
+        title: "project 1",
+        subtitel: "React",
+        description: "description about the project",
+        image: "img or gif",
+        link: "link to image"
+    },
+    {
+        title: "project 1",
+        subtitel: "React",
+        description: "description about the project",
+        image: "img or gif",
+        link: "link to image"
+    },
+]
+
+export const skills = [
+    "javascript", "react", "tailwind"
+]
