777d04b9fb25e75335817a929aa7261c54529ed2
john meng <john.meng014@gmail.com>
2022-06-14 15:16:33 -0400

    updated the about me page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import React from 'react'
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <main className="text-gray-400 bg-gray-800 body-font">
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Contact />
+    </main>
   )
 }
