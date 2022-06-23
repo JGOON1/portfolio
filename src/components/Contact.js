12195f69d009760c012a7ed9ef37535ed831bc28
john meng <john.meng014@gmail.com>
2022-06-22 20:40:05 -0400

    added contacts section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,6 +2,16 @@ import React from 'react'
 
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <section id='contact' className='text-grey-400 bg-gray-900 body-font'>
+      <div className='container px-5 py-10 mx-auto'>
+        <div className='text-center mb-20'>
+          <h1 className='sm:text-4xl text-3xl font-medium title-font text-white mb-4'>Connect with me!</h1>
+          <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
+            Feel free to contact me for questions!
+          </p>
+          
+        </div>
+      </div>
+    </section>
   )
 }
